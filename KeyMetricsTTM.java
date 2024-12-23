8468a88acb90484d950cb2e47e312101a82c10a3
FrancescoAstuti <francesco.astuti@outlook.com>
2024-12-23 22:39:43 +0100

    Add files via upload

--- KeyMetricsTTM.java
+++ KeyMetricsTTM.java
@@ -1,12 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.JSONObject;
 
-/**
- *
- * @author Francesco
- */
 public class KeyMetricsTTM {
-    
+
+    private static final String API_URL = "https://financialmodelingprep.com/api/v3/key-metrics-ttm/";
+
+    public static String getEPSTTM(String ticker) throws IOException {
+        String urlString = API_URL + ticker;
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        int responseCode = conn.getResponseCode();
+        
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        } else {
+            String inline = "";
+            Scanner scanner = new Scanner(url.openStream());
+
+            // Write all the JSON data into a string using a scanner
+            while (scanner.hasNext()) {
+                inline += scanner.nextLine();
+            }
+
+            // Close the scanner
+            scanner.close();
+
+            // Using the JSON simple library parse the string into a JSON object
+            JSONObject jsonObject = new JSONObject(inline);
+
+            // Get the EPS TTM from the JSON object
+            return jsonObject.getJSONArray("metrics").getJSONObject(0).getString("epsTTM");
+        }
+    }
 }
+
