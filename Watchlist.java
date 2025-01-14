93bd7363429267aaa182191c9483de61a5015e53
FrancescoAstuti <francesco.astuti@outlook.com>
2025-01-14 20:07:28 +0100

    Add files via upload

--- Watchlist.java
+++ Watchlist.java
@@ -725,14 +725,7 @@ public class Watchlist {
         } else {
             debtToEquityTerm = 1 / (double) debtToEquity;
         }
-        double payoutRatioTerm;
-        if (payoutRatio == 0) {  // Since payoutRatio is already a double, just compare it directly
-            payoutRatioTerm = 0;
-        } else {
-            payoutRatioTerm = 1 / payoutRatio;
-    }
-        
-        return 2*(pbAvg / pbTtm) + 2*(peAvg / peTtm) + (payoutRatioTerm) + (debtToEquityTerm) + (5 * roe) + (10 * dividendYield);
+        return 2*(pbAvg / pbTtm) + 2*(peAvg / peTtm) + (1 / payoutRatio) + debtToEquityTerm + 5 * roe + 20 * dividendYield;
     }
 
     public static void main(String[] args) {
